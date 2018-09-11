01a85e0ebd6d1fd9cd699c76d06d5de7d126abab
vibhu mahendru <vibhumahendru@gmail.com>
2018-09-11 21:34:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function removeFromCart(item){
         var newCart = cart.splice(i,1)
         return cart;
       }
-      else return"That item is not in your cart."
+     // else return "That item is not in your cart."
     }
   
 }
